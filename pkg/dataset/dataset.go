107c4e523733e0bbea66cf96a41c425bc931f609
ukpabik <kelechiukp@gmail.com>
2026-05-30 09:05:10 -0400

    feat: add initial feature and dataset types

---
+++ pkg/dataset/dataset.go
@@ -0,0 +1,5 @@
+package dataset
+
+type Dataset struct {
+	Features []Feature
+}
---
+++ pkg/dataset/feature.go
@@ -0,0 +1,42 @@
+package dataset
+
+type FeatureType int
+
+const (
+	BinaryType FeatureType = iota
+	CategoricalType
+	Float64Type
+)
+
+type Feature interface {
+	GetName() string
+	GetType() FeatureType
+}
+
+type BinaryFeature struct {
+	Name   string
+	Type   FeatureType
+	Values []uint8
+}
+
+func (bf *BinaryFeature) GetName() string      { return bf.Name }
+func (bf *BinaryFeature) GetType() FeatureType { return BinaryType }
+
+// Mapped using One-Hot encoding
+type CategoricalFeature struct {
+	Name   string
+	Type   FeatureType
+	Values []uint8
+}
+
+func (cf *CategoricalFeature) GetName() string      { return cf.Name }
+func (cf *CategoricalFeature) GetType() FeatureType { return CategoricalType }
+
+type Float64Feature struct {
+	Name   string
+	Type   FeatureType
+	Values []float64
+}
+
+func (ff *Float64Feature) GetName() string      { return ff.Name }
+func (ff *Float64Feature) GetType() FeatureType { return Float64Type }
